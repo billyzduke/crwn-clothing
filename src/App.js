0d97c24bc9ac0d12d3ef486ab6e5ad4367cc1344
Billy Z Duke <billyzduke@gmail.com>
2020-04-21 17:53:51 -0700

    Lesson 13: Redirect to home / forbid auth page when signed in, UserActionTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import 'App.scss'
@@ -10,8 +10,6 @@ import ShopPage from 'pages/shop'
 import AuthPage from 'pages/auth'
 import { auth, createUserProfileDoc } from 'firebase-utils'
 import { setCurrentUser } from 'reduxxx/user/actions'
-
-const AcctPage = null
 
 class App extends React.Component {
   unsubscribeFromAuth = null
@@ -52,8 +50,17 @@ class App extends React.Component {
             component={ ShopPage }
           />
           <Route
+            exact
             path="/auth"
-            component={ this.props.currentUser ? AcctPage : AuthPage }
+            render={ () =>
+              this.props.currentUser ? (
+                <Redirect
+                  to='/'
+                />
+              ) : (
+                <AuthPage />
+              )
+            }
           />
         </Switch>
       </div>
@@ -61,8 +68,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(App)
